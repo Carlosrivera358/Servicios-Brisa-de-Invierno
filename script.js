9ca4ff5e6b03cbd3bd1a00639ce3a74f81d68351
Carlosrivera358 <carlitosnomegusta@gmail.com>
2024-10-23 19:08:47 -0500

    Actualizar

---
+++ script.js
@@ -0,0 +1,2 @@
+// Aquí puedes agregar funciones JS en el futuro si es necesario.
+// Por el momento, no hay interacciones dinámicas, pero si decides agregar funcionalidad interactiva, puedes usar este archivo.
